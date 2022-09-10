b43d96a14458270e9b4fbbdebf6db66a7caf7056
qsqnk <st087201@student.spbu.ru>
2022-09-10 03:05:59 +0300

    Add docs

--- project/graph_utils.py
+++ project/graph_utils.py
@@ -3,6 +3,7 @@ from typing import NamedTuple, Set, Tuple, Union
 import cfpq_data
 import networkx.drawing.nx_pydot
 from networkx import MultiDiGraph
+from typing.io import IO
 
 __all__ = [
     "GraphInfo",
@@ -12,16 +13,39 @@ __all__ = [
     "save_graph",
 ]
 
-from typing.io import IO
-
 
 class GraphInfo(NamedTuple):
+    """Class represents holder of general information about graph.
+
+    Attributes
+    ----------
+
+    number_of_nodes : int
+        Number of nodes stored in the graph.
+    number_of_edges : int
+        Number of edges stored in the graph.
+    edge_labels : Set[str]
+        All labels of edges.
+    """
+
     number_of_nodes: int
     number_of_edges: int
     edge_labels: Set[str]
 
 
 def graph_info_of(graph: MultiDiGraph) -> GraphInfo:
+    """Calculates information about graph.
+
+    Parameters
+    ----------
+    graph : MultiDiGraph
+        Graph to be processed
+
+    Returns
+    -------
+    graph_info : GraphInfo
+        Info about graph
+    """
     edge_labels = set(label for _, _, label in graph.edges(data="label") if label)
     return GraphInfo(
         number_of_nodes=graph.number_of_nodes(),
@@ -35,6 +59,23 @@ def build_two_cycle_labeled_graph(
     size_of_second_cycle: int,
     edge_labels: Tuple[str, str],
 ) -> MultiDiGraph:
+    """Builds labeled graph with two cycles.
+
+    Parameters
+    ----------
+    size_of_first_cycle : int
+        Count of nodes in the first cycle without common node
+    size_of_second_cycle : int
+        Count of nodes in the second cycle without common node
+    edge_labels : Tuple[str, str]
+        Edge labels of graph
+
+    Returns
+    -------
+    graph : MultiDiGraph
+        Created graph
+    """
+
     return cfpq_data.labeled_two_cycles_graph(
         n=size_of_first_cycle,
         m=size_of_second_cycle,
@@ -43,9 +84,35 @@ def build_two_cycle_labeled_graph(
 
 
 def load_graph(graph_name: str) -> MultiDiGraph:
+    """Loads a graph by name.
+
+    Parameters
+    ----------
+    graph_name : str
+        The name of graph in dataset
+
+    Returns
+    -------
+    graph : MultiDiGraph
+        Loaded graph.
+    """
     graph_path = cfpq_data.download(graph_name)
-    return cfpq_data.graph_from_csv(graph_path)
+    graph = cfpq_data.graph_from_csv(graph_path)
+    return graph
 
 
 def save_graph(graph: MultiDiGraph, file: Union[str, IO]) -> None:
+    """Saves a graph into a file.
+
+    Parameters
+    ----------
+    graph : MultiDiGraph
+        Graph to be saved
+    file : Union[str, IO]
+        The name of file or file itself
+
+    Returns
+    -------
+    None
+    """
     networkx.drawing.nx_pydot.write_dot(graph, file)
