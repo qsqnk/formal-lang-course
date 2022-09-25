d17e5a29d1c59bb9137f9f75ee61bec0ea53487c
qsqnk <st087201@student.spbu.ru>
2022-09-25 05:24:01 +0300

    Add nfa to bool matrix conversion and transitive closure method

---
+++ project/matrix_utils.py
@@ -0,0 +1,93 @@
+__all__ = [
+    "BoolMatrix",
+]
+
+from typing import Dict, Set, Any
+
+from pyformlang.finite_automaton import State, EpsilonNFA
+from scipy.sparse import dok_matrix, kron
+
+
+class BoolMatrix:
+    # Only for internal use
+    def __init__(
+        self,
+        state_to_idx: Dict[Any, int],
+        start_states: Set[Any],
+        final_states: Set[Any],
+        b_mtx: Dict[Any, dok_matrix],
+    ):
+        self.state_to_idx = state_to_idx
+        self.start_states = start_states
+        self.final_states = final_states
+        self.b_mtx = b_mtx
+
+    def __and__(self, other: "BoolMatrix") -> "BoolMatrix":
+        inter_labels = self.b_mtx.keys() & other.b_mtx.keys()
+        inter_b_mtx = {
+            label: kron(self.b_mtx[label], other.b_mtx[label]) for label in inter_labels
+        }
+        inter_state_to_idx = dict()
+        inter_start_states = set()
+        inter_final_states = set()
+        for self_state, self_idx in self.state_to_idx.items():
+            for other_state, other_idx in other.state_to_idx.items():
+                state = (self_state, other_state)
+                idx = self_idx * len(other.state_to_idx) + other_idx
+                inter_state_to_idx[state] = idx
+                if (
+                    self_state in self.start_states
+                    and other_state in other.start_states
+                ):
+                    inter_start_states.add(state)
+                if (
+                    self_state in self.final_states
+                    and other_state in other.final_states
+                ):
+                    inter_final_states.add(state)
+        return BoolMatrix(
+            state_to_idx=inter_state_to_idx,
+            start_states=inter_start_states,
+            final_states=inter_final_states,
+            b_mtx=inter_b_mtx,
+        )
+
+    def transitive_closure(self) -> dok_matrix:
+        transitive_closure = sum(
+            self.b_mtx.values(),
+            start=dok_matrix((len(self.state_to_idx), len(self.state_to_idx))),
+        )
+        prev_nnz, cur_nnz = None, transitive_closure.nnz
+        if not cur_nnz:
+            return transitive_closure
+        while prev_nnz != cur_nnz:
+            transitive_closure += transitive_closure @ transitive_closure
+            prev_nnz, cur_nnz = cur_nnz, transitive_closure.nnz
+        return transitive_closure
+
+    @classmethod
+    def from_nfa(cls, nfa: EpsilonNFA) -> "BoolMatrix":
+        state_to_idx = {state: idx for idx, state in enumerate(nfa.states)}
+        return cls(
+            state_to_idx=state_to_idx,
+            start_states=nfa.start_states.copy(),
+            final_states=nfa.final_states.copy(),
+            b_mtx=cls._b_mtx_from_nfa(
+                nfa=nfa,
+                state_to_idx=state_to_idx,
+            ),
+        )
+
+    @staticmethod
+    def _b_mtx_from_nfa(
+        nfa: EpsilonNFA, state_to_idx: Dict[State, int]
+    ) -> Dict[Any, dok_matrix]:
+        b_mtx = dict()
+        state_from_to_transition = nfa.to_dict()
+        for label in nfa.symbols:
+            dok_mtx = dok_matrix((len(nfa.states), len(nfa.states)), dtype=bool)
+            for state_from, transitions in state_from_to_transition.items():
+                for state_to in transitions.get(label, {}):
+                    dok_mtx[state_to_idx[state_from], state_to_idx[state_to]] = True
+            b_mtx[label] = dok_mtx
+        return b_mtx
