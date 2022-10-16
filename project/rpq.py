a337da307219563e6df6668cb95c328a6fc7167d
qsqnk <st087201@student.spbu.ru>
2022-10-16 03:01:05 +0300

    Remove pycubool tests

--- project/rpq.py
+++ project/rpq.py
@@ -1,6 +1,6 @@
 import enum
 import sys
-from typing import Set, Optional, Tuple, Any
+from typing import Set, Optional, Tuple, Any, Callable
 
 from networkx import MultiDiGraph
 from pyformlang.regular_expression import Regex
@@ -20,63 +20,6 @@ __all__ = [
     "MultipleSourceRpqMode",
     "MatrixType",
 ]
-
-
-def rpq_tensor(
-    graph: MultiDiGraph,
-    query: Regex,
-    start_states: Optional[Set],
-    final_states: Optional[Set],
-) -> Set[Tuple[Any, Any]]:
-    """Executes regular query on graph using tensor multiplication
-
-    Parameters
-    ----------
-    graph : MultiDiGraph
-        The graph on which query will be executed
-    query: Regex
-        Query represented by regular expression
-    start_states: Optional[Set]
-        Set of nodes of the graph that will be treated as start states in NFA
-        If parameter is None then each graph node is considered the start state
-    final_states: Optional[Set]
-        Set of nodes of the graph that will be treated as final states in NFA
-        If parameter is None then each graph node is considered the final state
-
-    Returns
-    -------
-    result : Set[Tuple[Any, Any]]
-        The set of pairs where the node in second place is reachable
-         from the node in first place with a constraint on a given query
-    """
-    nfa_bool_mtx = BoolMatrixAutomaton.from_nfa(
-        graph_to_epsilon_nfa(
-            graph=graph,
-            start_states=start_states,
-            final_states=final_states,
-        )
-    )
-    query_bool_mtx = BoolMatrixAutomaton.from_nfa(
-        regex_to_min_dfa(regex=query),
-    )
-    intersection_bool_mtx = nfa_bool_mtx & query_bool_mtx
-    idx_to_state = {
-        idx: state for state, idx in intersection_bool_mtx.state_to_idx.items()
-    }
-    transitive_closure = intersection_bool_mtx.transitive_closure()
-    result = set()
-    for state_from_idx, state_to_idx in zip(*transitive_closure.nonzero()):
-        state_from, state_to = idx_to_state[state_from_idx], idx_to_state[state_to_idx]
-        if (
-            state_from in intersection_bool_mtx.start_states
-            and state_to in intersection_bool_mtx.final_states
-        ):
-            state_from_graph_value, _ = state_from.value
-            state_to_graph_value, _ = state_to.value
-            result.add(
-                (state_from_graph_value, state_to_graph_value),
-            )
-    return result
 
 
 class MultipleSourceRpqMode(enum.Enum):
@@ -109,6 +52,76 @@ class MatrixType(enum.Enum):
 
     SCIPY = enum.auto()
     PYCUBOOL = enum.auto()
+
+
+def nfa_to_mtx_converter(matrix_type: MatrixType) -> Callable:
+    if matrix_type == MatrixType.SCIPY:
+        return lambda nfa: BoolMatrixAutomaton.from_nfa(nfa)
+    else:
+        if sys.platform != "linux":
+            raise RuntimeError("Unsupported operating system")
+        return lambda nfa: BoolMatrixAutomatonPyCuBool.from_nfa(nfa)
+
+
+def rpq_tensor(
+    graph: MultiDiGraph,
+    query: Regex,
+    start_states: Optional[Set],
+    final_states: Optional[Set],
+    matrix_type: MatrixType,
+) -> Set[Tuple[Any, Any]]:
+    """Executes regular query on graph using tensor multiplication
+
+    Parameters
+    ----------
+    graph : MultiDiGraph
+        The graph on which query will be executed
+    query: Regex
+        Query represented by regular expression
+    start_states: Optional[Set]
+        Set of nodes of the graph that will be treated as start states in NFA
+        If parameter is None then each graph node is considered the start state
+    final_states: Optional[Set]
+        Set of nodes of the graph that will be treated as final states in NFA
+        If parameter is None then each graph node is considered the final state
+    matrix_type: MatrixType
+        The type of sparse matrices used in sync bfs
+
+    Returns
+    -------
+    result : Set[Tuple[Any, Any]]
+        The set of pairs where the node in second place is reachable
+         from the node in first place with a constraint on a given query
+    """
+    converter = nfa_to_mtx_converter(matrix_type)
+    nfa_bool_mtx = converter(
+        graph_to_epsilon_nfa(
+            graph=graph,
+            start_states=start_states,
+            final_states=final_states,
+        )
+    )
+    query_bool_mtx = converter(
+        regex_to_min_dfa(regex=query),
+    )
+    intersection_bool_mtx = nfa_bool_mtx & query_bool_mtx
+    idx_to_state = {
+        idx: state for state, idx in intersection_bool_mtx.state_to_idx.items()
+    }
+    transitive_closure = intersection_bool_mtx.transitive_closure()
+    result = set()
+    for state_from_idx, state_to_idx in zip(*transitive_closure.nonzero()):
+        state_from, state_to = idx_to_state[state_from_idx], idx_to_state[state_to_idx]
+        if (
+            state_from in intersection_bool_mtx.start_states
+            and state_to in intersection_bool_mtx.final_states
+        ):
+            state_from_graph_value, _ = state_from.value
+            state_to_graph_value, _ = state_to.value
+            result.add(
+                (state_from_graph_value, state_to_graph_value),
+            )
+    return result
 
 
 def rpq_bfs(
@@ -146,19 +159,15 @@ def rpq_bfs(
         if mode is FIND_REACHABLE_FOR_EACH_START_NODE -- set of tuples (U, V)
         where U is start node and V is final node reachable from U
     """
-    mtx_creator = (
-        lambda nfa: BoolMatrixAutomatonPyCuBool.from_nfa(nfa)
-        if matrix_type == MatrixType.PYCUBOOL
-        else BoolMatrixAutomaton.from_nfa(nfa)
-    )
-    nfa_bool_mtx = mtx_creator(
+    converter = nfa_to_mtx_converter(matrix_type)
+    nfa_bool_mtx = converter(
         graph_to_epsilon_nfa(
             graph=graph,
             start_states=start_states,
             final_states=final_states,
         )
     )
-    query_bool_mtx = mtx_creator(
+    query_bool_mtx = converter(
         regex_to_min_dfa(regex=query),
     )
     return nfa_bool_mtx.sync_bfs(
