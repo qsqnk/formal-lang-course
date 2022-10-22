43daeb3fdc2382822ef7bcba2924c6eb477d7768
qsqnk <st087201@student.spbu.ru>
2022-10-22 22:53:58 +0300

    Add docs

--- project/cfg_utils.py
+++ project/cfg_utils.py
@@ -10,6 +10,18 @@ __all__ = [
 
 
 def cfg_to_wcnf(cfg: CFG) -> CFG:
+    """Converts CFG to Weak Chomsky Normal Form
+
+    Parameters
+    ----------
+    cfg : CFG
+        Context free grammar
+
+    Returns
+    -------
+    wcnf: CFG
+        Converted cfg
+    """
     cleared = (
         cfg.remove_useless_symbols()
         .eliminate_unit_productions()
@@ -21,6 +33,22 @@ def cfg_to_wcnf(cfg: CFG) -> CFG:
     return CFG(start_symbol=cleared.start_symbol, productions=set(productions))
 
 
-def cfg_from_file(file: Union[str, IO], start_symbol=Variable("S")) -> CFG:
+def cfg_from_file(
+    file: Union[str, IO], start_symbol: Union[str, Variable] = Variable("S")
+) -> CFG:
+    """Loads CFG from file
+
+    Parameters
+    ----------
+    file : Union[str, IO]
+        Filename or file itself
+    start_symbol : Union[str, Variable]
+        The start symbol for the CFG to be loaded
+
+    Returns
+    -------
+    cfg: CFG
+        Loaded CFG
+    """
     with open(file) as f:
         return CFG.from_text(f.read(), start_symbol=start_symbol)
