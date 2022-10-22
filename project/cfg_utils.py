d8bc3d6a957c1cf3f01a6d04054f205e6022206f
qsqnk <st087201@student.spbu.ru>
2022-10-22 22:42:59 +0300

    Add to wcnf converter & add cfg from file loader & add tests

---
+++ project/cfg_utils.py
@@ -0,0 +1,27 @@
+from typing import Union
+
+from pyformlang.cfg import CFG, Variable
+
+__all__ = [
+    "cfg_to_wcnf",
+    "cfg_from_file",
+]
+
+from typing.io import IO
+
+
+def cfg_to_wcnf(cfg: CFG) -> CFG:
+    cleared = (
+        cfg.remove_useless_symbols()
+        .eliminate_unit_productions()
+        .remove_useless_symbols()
+    )
+    productions = cleared._decompose_productions(
+        cleared._get_productions_with_only_single_terminals()
+    )
+    return CFG(start_symbol=cleared.start_symbol, productions=set(productions))
+
+
+def cfg_from_file(file: Union[str, IO], start_symbol=Variable("S")) -> CFG:
+    with open(file) as f:
+        return CFG.from_text(f.read(), start_symbol=start_symbol)
